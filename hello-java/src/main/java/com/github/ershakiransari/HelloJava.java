4ba1c3a92fe705778301655178d3f5b864a97d76
Shakir Ansari <shakir.ansari@live.com>
2022-09-23 23:57:53 +0530

    pluginManagement - maven-jar-plugin

--- hello-java/src/main/java/com/github/ershakiransari/HelloJava.java
+++ hello-java/src/main/java/com/github/ershakiransari/HelloJava.java
@@ -2,14 +2,15 @@ package com.github.ershakiransari;
 
 /**
  * @author Shakir
+ * @version 2022-09-23
  */
-public class HelloMaven
+public class HelloJava
 {
 	public static void main(String[] args)
 	{
 		// version: will be available on package phase
 		// plugin: https://mvnrepository.com/artifact/org.apache.maven.plugins/maven-jar-plugin
-		System.out.println(HelloMaven.class.getName());
-		System.out.println("v" + HelloMaven.class.getPackage().getImplementationVersion());
+		System.out.println(HelloJava.class.getName());
+		System.out.println("v" + HelloJava.class.getPackage().getImplementationVersion());
 	}
 }
